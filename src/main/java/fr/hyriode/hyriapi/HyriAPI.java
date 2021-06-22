4d14ce3bb69cff4fe78564be2195030cb3e1ba1f
KeinzTv <keinzyt@gmail.com>
2021-06-22 19:24:15 +0200

    Setup base project

---
+++ src/main/java/fr/hyriode/hyriapi/HyriAPI.java
@@ -0,0 +1,17 @@
+package fr.hyriode.hyriapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HyriAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
