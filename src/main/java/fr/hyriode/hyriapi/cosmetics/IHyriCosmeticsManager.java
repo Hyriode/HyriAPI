bf07f612262298de5b05dcaebebbebeb645025ab
Yggdrasil80 <louisdechorivit@gmail.com>
2021-10-23 22:17:31 +0200

    Apply Fixes to CosmeticsManager Part

--- src/main/java/fr/hyriode/hyriapi/cosmetics/IHyriCosmeticsManager.java
+++ src/main/java/fr/hyriode/hyriapi/cosmetics/IHyriCosmeticsManager.java
@@ -41,35 +41,35 @@ public interface IHyriCosmeticsManager {
      * @param uuid The player UUID
      * @return True if player has the given cosmetic
      */
-    boolean hasCosmetic(IHyriCosmetic cosmetic, UUID uuid);
+    boolean hasCosmetic(Class<? extends IHyriCosmetic> cosmetic, UUID uuid);
 
     /**
      * Add a cosmetic to a player
      * @param cosmetic The cosmetic to give
      * @param uuid The player to give cosmetic
      */
-    void addCosmetic(IHyriCosmetic cosmetic, UUID uuid);
+    void addCosmetic(Class<? extends IHyriCosmetic> cosmetic, UUID uuid);
 
     /**
      * Add multiples cosmetics to a player
      * @param cosmetics List of cosmetics to give
      * @param uuid The player to give cosmetics
      */
-    void addCosmetics(List<IHyriCosmetic> cosmetics, UUID uuid);
+    void addCosmetics(List<Class<? extends IHyriCosmetic>> cosmetics, UUID uuid);
 
     /**
      * Remove a cosmetic to a player
      * @param cosmetic The cosmetic to remove
      * @param uuid The player to remove cosmetic
      */
-    void removeCosmetic(IHyriCosmetic cosmetic, UUID uuid);
+    void removeCosmetic(Class<? extends IHyriCosmetic> cosmetic, UUID uuid);
 
     /**
      * Remove multiples cosmetics to a player
      * @param cosmetics List of cosmetics to remove
      * @param uuid The player to remove cosmetics
      */
-    void removeCosmetics(List<IHyriCosmetic> cosmetics, UUID uuid);
+    void removeCosmetics(List<Class<? extends IHyriCosmetic>> cosmetics, UUID uuid);
 
     /**
      * Get all registered cosmetics
@@ -81,5 +81,5 @@ public interface IHyriCosmeticsManager {
      * Stop a cosmetic for all players
      * @param cosmetic The cosmetic to stop
      */
-    void stopForAllPlayers(IHyriCosmetic cosmetic);
+    void stopForAllPlayers(Class<? extends IHyriCosmetic> cosmetic);
 }
