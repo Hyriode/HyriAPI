21d270f3afe022a17c127ccef30a425349d5bbdc
AstFaster <astfaster@gmail.com>
2023-02-05 12:41:18 +0100

    Fix LobbyAPI and state check in listeners

--- Common/src/main/java/fr/hyriode/api/impl/common/hyggdrasil/listener/ProxiesListener.java
+++ Common/src/main/java/fr/hyriode/api/impl/common/hyggdrasil/listener/ProxiesListener.java
@@ -6,6 +6,7 @@ import fr.hyriode.hyggdrasil.api.event.HyggEventBus;
 import fr.hyriode.hyggdrasil.api.event.model.proxy.HyggProxyStartedEvent;
 import fr.hyriode.hyggdrasil.api.event.model.proxy.HyggProxyStoppedEvent;
 import fr.hyriode.hyggdrasil.api.event.model.proxy.HyggProxyUpdatedEvent;
+import fr.hyriode.hyggdrasil.api.proxy.HyggProxy;
 
 /**
  * Project: HyriAPI
@@ -24,7 +25,11 @@ public class ProxiesListener {
 
     public void register() {
         this.eventBus.subscribe(HyggProxyStartedEvent.class, event -> this.proxyManager.addProxy(event.getProxy()));
-        this.eventBus.subscribe(HyggProxyUpdatedEvent.class, event -> this.proxyManager.addProxy(event.getProxy()));
+        this.eventBus.subscribe(HyggProxyUpdatedEvent.class, event -> {
+            if (event.getProxy().getState() != HyggProxy.State.SHUTDOWN) {
+                this.proxyManager.addProxy(event.getProxy());
+            }
+        });
         this.eventBus.subscribe(HyggProxyStoppedEvent.class, event -> this.proxyManager.removeProxy(event.getProxy()));
     }
 
